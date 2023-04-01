f22c08273c287e69cd857580448906610a4a0a75
ziqiang <1694392889@qq.com>
2023-04-01 23:15:03 +0800

    feat: prepare component(#3)

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,8 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+      <HelloWorld msg="Hello World" />
     </div>
   </header>
 
---
+++ src/components/PinyinBlock.vue
@@ -0,0 +1,21 @@
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  name: 'PinYinBlock',
+  setup() {
+    const message = ref('this is in canvas');
+
+    function render() {
+      console.log('render called');
+    }
+
+    return { message, render };
+  }
+});
+</script>
+<template>
+  <div>
+    <p>{{ message }}</p>
+  </div>
+</template>
